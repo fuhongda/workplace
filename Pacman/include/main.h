5c931b70b7aa1549672c0f619e5b4537bdf37cd9
fhd <itsfhd@163.com>
2018-10-10 10:51:39 +0800

    Pacman

--- Pacman/include/main.h
+++ Pacman/include/main.h
@@ -11,24 +11,24 @@
 #include <stdlib.h>
 using namespace std;
 
-extern bool replay; //check if starts a new game
-extern bool over; //check for the game to be over
-extern float squareSize; //size of one square on the game
-extern float xIncrement; // x movement on pacman
-extern float yIncrement; // y movement on pacman
-extern int rotation; // orientation of pacman
-extern float* monster1; //coordinates and direction of first monster
-extern float* monster2; //coordinates and direction of second monster
-extern float* monster3; //coordinates and direction of third monster
-extern float* monster4; //coordinates and direction of fourth monster
-extern vector<int> border; //coordinates of the border walls
+extern bool replay; //检查是否启动游戏
+extern bool over; //检查游戏是否结束
+extern float squareSize; //一个单元大小
+extern float xIncrement; // x坐标
+extern float yIncrement; // y坐标
+extern int rotation; // 定位
+extern float* monster1; ////第一个怪物的坐标和方向
+extern float* monster2; //第二个怪物的坐标和方向
+extern float* monster3; //第三个怪物的坐标和方向
+extern float* monster4; //第四个怪物的坐标和方向
+extern vector<int> border; //墙坐标
 
-//coordinates of the obstacles (divided into 3 for clarity)
+//障碍物坐标 (为了清晰分为三个)
 extern vector<int> obstaclesTop;
 extern vector<int> obstaclesMiddle;
 extern vector<int> obstaclesBottom;
 extern deque<float> food;
-extern vector<vector<bool>> bitmap; // 2d image of which squares are blocked and which are clear for pacman to move in 
-extern bool* keyStates; // record of all keys pressed 
-extern int points; // total points collected
+extern vector<vector<bool>> bitmap; // 2d图像，可移动区域
+extern bool* keyStates; // 按键记录
+extern int points; // 得分
 #endif
