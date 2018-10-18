d39f8137fa66a696c79a425ba6a45de9224e0a8e
fuhongda <itsfhd@163.com>
2018-10-18 14:04:10 +0800

    修改实验四

--- mySTL/Test/functionaltest.cpp
+++ mySTL/Test/functionaltest.cpp
@@ -27,13 +27,13 @@ void print(int i,int j)//普通函数对象
 
 int main(){
 
-    vector <int> vec;
+    mySTL::vector <int> vec;
     for (int i = 0; i < 10; i++)
     {
         vec.push_back(i + 1);
     }
 
-    vector<int>::iterator it = find_if(vec.begin(), vec.end(), bind2nd(compare(),6));//找出大于6的第一个数
+    mySTL::vector<int>::iterator it = mySTL::find_if(vec.begin(), vec.end(), bind2nd(compare(),6));//找出大于6的第一个数
     if (it == vec.end())
     {
         cout << "cannot find the number!" << endl;
@@ -43,7 +43,7 @@ int main(){
         cout << "find num: " << *it << endl;
     }
 
-    vector<int>::iterator rit = find_if(vec.begin(), vec.end(), not1(comparetonum()));  //取反适配器的用法，找出小于5的第一个数
+    mySTL::vector<int>::iterator rit = mySTL::find_if(vec.begin(), vec.end(), not1(comparetonum()));  //取反适配器的用法，找出小于5的第一个数
     if (rit == vec.end())
     {
         cout << "cannot find the number!" << endl;
@@ -53,13 +53,13 @@ int main(){
         cout << "find num: " << *rit << endl;
     }
 
-    vector<int> vec1;
+    mySTL::vector<int> vec1;
     for (int i = 0; i < 10; i++)
     {
         vec1.push_back(i);
     }
     
-    for_each(vec1.begin(), vec1.end(), bind2nd(ptr_fun(print),5)); //使用ptr_fun将普通函数转换为函数对象，然后给函数对象绑定参数。
+    mySTL::for_each(vec1.begin(), vec1.end(), bind2nd(ptr_fun(print),5)); //使用ptr_fun将普通函数转换为函数对象，然后给函数对象绑定参数。
     cout << endl;
     
     return 0;
